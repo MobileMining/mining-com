953167f1a89ed35fc5aec13977d842bbd8608659
Ian Muirhead <ianmuirhead@gmail.com>
2015-04-07 22:17:53 +0000

    Changes from Screen Builder

---
+++ AppBuilder1/data/defaultProvider.js
@@ -0,0 +1,12 @@
+'use strict';
+
+(function() {
+    //create a reference for the data provider to be used throughout the app
+
+    app.data.defaultprovider = new Everlive({
+        apiKey: 'o5TbRF5RwyenKCve',
+
+        scheme: 'https'
+    });
+
+}());
